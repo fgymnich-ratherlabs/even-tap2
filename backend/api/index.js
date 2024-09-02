22f3d79da9f16b706401f3116f1e1f0eca32ddc5
fgymnich-rather <fgymnich@ratherlabs.com>
2024-09-02 12:55:56 -0300

    vercel and onRender connection 2

--- backend/api/index.js
+++ backend/api/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 // Configurar CORS
 app.use(cors({
-  origin: 'https://even-tap2.onrender.com', // Cambia esto al dominio de tu frontend
+  origin: 'https://even-tap2.vercel.app', // Cambia esto al dominio de tu frontend
   methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
   credentials: true // Permitir el uso de cookies y headers de autenticación
