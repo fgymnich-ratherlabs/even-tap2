6af3ad3ba097e9b444ce0272ad08c6fdd4616298
fgymnich-rather <fgymnich@ratherlabs.com>
2024-09-04 16:34:01 -0300

    changed url variables to web

--- backend/api/index.js
+++ backend/api/index.js
@@ -27,8 +27,8 @@ app.use(express.json());
 
 // Configurar CORS
 app.use(cors({
-  //origin: 'https://even-tap2.vercel.app', // Cambia esto al dominio de tu frontend
-  origin: 'http://localhost:3000',
+  origin: 'https://even-tap2.vercel.app', // Cambia esto al dominio de tu frontend
+  //origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
   credentials: true // Permitir el uso de cookies y headers de autenticación
