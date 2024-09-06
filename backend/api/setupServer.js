61327ba94fbdd1155146e851e473c8900e1108a5
fgymnich-rather <fgymnich@ratherlabs.com>
2024-09-06 14:12:44 -0300

    dependency injection

---
+++ backend/api/setupServer.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+
+async function setupServer({ schema, authenticateMiddleware, root }) {
+    const app = express();
+
+    app.use(express.json());
+
+    // Configurar CORS
+    app.use(cors({
+      //origin: 'https://even-tap2.vercel.app', // Cambia esto al dominio de tu frontend
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
+      allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+      credentials: true // Permitir el uso de cookies y headers de autenticación
+    }));
+
+    //graphQLHTTP middleware
+    app.use('/graphql', authenticateMiddleware, graphqlHTTP((req) => ({
+      schema,
+      rootValue: root,
+      context: req,
+      graphiql: true,
+    })));
+
+    return app;
+}
+
+module.exports  = setupServer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,3 @@
-import { redirect } from 'next/navigation'
 
 export default function Home() {
-  //if (!login) {redirect('/login')}
-  redirect('/signin');
-
 }
